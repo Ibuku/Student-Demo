818f79620f9615405e5dc68a94d256b426997f65
ibukun_babatunde@outlook.com <ibukun_babatunde@outlook.com>
2019-12-04 12:03:00 -0800

    Add project files.

---
+++ IService1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ServiceModel;
+using StudentDemo.BO.Request;
+using StudentDemo.BO.Response;
+using System.Collections.Generic;
+
+
+namespace StudentDemo
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+
+
+        [OperationContract]
+        StudentResponse AddStudent(StudentResponse student);
+
+        [OperationContract]
+        List<StudentRequest> GetAll(StudentResponse student);
+
+       [OperationContract]
+        StudentResponse GetById(int Id);
+
+        [OperationContract]
+        int UpdateStudent(StudentRequest student);
+
+        [OperationContract]
+        int DeleteStudent(int studentId);
+
+        // TODO: Add your service operations here
+    }
+}
+
---
+++ Service1.svc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using StudentDemo.BO.Response;
+using StudentDemo.BO.Request;
+using StudentDemo.BLL;
+using StudentDemo.DAL;
+
+
+
+
+namespace StudentDemo
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+
+        StudentBLL BLL = new StudentBLL();
+        public StudentResponse AddStudent(StudentResponse student)
+        {
+            return BLL.AddStudent(student);
+        }
+
+        public List<StudentRequest> GetAll(StudentResponse student)
+        {
+            return BLL.GetAll(student);
+        }
+
+        public StudentResponse GetById(int studentId)
+        {
+            return BLL.GetById(studentId);
+        }
+        public int UpdateStudent(StudentRequest student)
+        {
+            return BLL.UpdateStudent(student);
+        }
+       
+        public int DeleteStudent(int studentId)
+        {
+            return BLL.DeleteStudent(studentId);
+        }
+    }
+}
+
